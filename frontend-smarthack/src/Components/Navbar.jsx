af391c1e4887fb00eb25d013358845875d8ea03e
Tiberiu Popescu <tiberiu1204@gmail.com>
2023-11-11 16:20:58 +0200

    Developed Navbar Component

--- frontend-smarthack/src/Components/Navbar.jsx
+++ frontend-smarthack/src/Components/Navbar.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import "./Navbar.css";
 
-const Navbar = () => {
+const Navbar = ( { links } ) => {
+  
   return (
     <section className='navbar'>
         <div className="navbar-wrapper">
             <div className="navbar-content-wrapper">
-                <div className="navbar-logo-wrapepr"></div>
-                <div className="navbar-links-wrapper"></div>
+                <div className="navbar-logo-wrapper"></div>
+                <div className="navbar-links-wrapper">
+                    {links.map((link, index) => {
+                        return <div className="navbar-link-wrapper" key={index}>
+                                    <a href={link.link} className="navbar-link">{link.text}</a>
+                                </div>
+                    })}
+                </div>
             </div>
         </div>
     </section>
--- frontend-smarthack/src/pages/Home.jsx
+++ frontend-smarthack/src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import Button from "../Components/Button";
+import Navbar from '../Components/Navbar';
 
 const Home = () => {
-  return (
-    <div></div>
+    const navLinks = [
+        { text: "Home", link: "home"},
+        { text: "Statistics", link: "statistics"},
+        { text: "Tasks", link: "tasks"},
+        { text: "Profile", link: "profile"},
+    ];
+    
+    return (
+    <>
+        <Navbar links={navLinks}/>
+    </>
   )
 }
 
