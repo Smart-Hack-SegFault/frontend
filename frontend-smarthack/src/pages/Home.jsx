0469c3127798e80f93b6007814e0ed14461fc422
Tiberiu Popescu <tiberiu1204@gmail.com>
2023-11-11 16:36:58 +0200

    idk

--- frontend-smarthack/src/pages/Home.jsx
+++ frontend-smarthack/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from "../Components/Button";
 import Navbar from '../Components/Navbar';
 
-const Home = () => {
+const Home = ({ userName }) => {
     const navLinks = [
         { text: "Home", link: "home"},
         { text: "Statistics", link: "statistics"},
@@ -13,6 +13,9 @@ const Home = () => {
     return (
     <>
         <Navbar links={navLinks}/>
+        <section className="section">
+            
+        </section>
     </>
   )
 }
